a7b3d9d82a624cd74cf1f6a130f578b0e1a4b43d
PimAkker <pimjanvandenakker@gmail.com>
2023-12-30 22:38:59 +0100

    add chair

--- blender_python_code/object_placement_utils.py
+++ blender_python_code/object_placement_utils.py
@@ -70,8 +70,7 @@ class object_placement:
         bpy.context.object.modifiers["GeometryNodes"]["Input_2"] = nr_of_legs
         bpy.context.object.modifiers["GeometryNodes"]["Input_5"] = size_x
         bpy.context.object.modifiers["GeometryNodes"]["Input_6"] = size_y
-            
-        
+
         
         bpy.ops.object.duplicate_move_linked(OBJECT_OT_duplicate={"linked":True, "mode":'TRANSLATION'}, TRANSFORM_OT_translate={"value":(0,0,0), "orient_type":'GLOBAL', "orient_matrix":((1, 0, 0), (0, 1, 0), (0, 0, 1)), "orient_matrix_type":'GLOBAL', "constraint_axis":(False, False, False), "mirror":False, "use_proportional_edit":False, "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "use_proportional_connected":False, "use_proportional_projected":False, "snap":False, "snap_elements":{'FACE_NEAREST'}, "use_snap_project":True, "snap_target":'CLOSEST', "use_snap_self":True, "use_snap_edit":True, "use_snap_nonedit":True, "use_snap_selectable":False, "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "cursor_transform":False, "texture_space":False, "remove_on_cancel":False, "view2d_edge_pan":False, "release_confirm":False, "use_accurate":False, "use_automerge_and_split":False})
         obj = bpy.context.active_object
