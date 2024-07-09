9370083cff4dcb497a2c237e14c055ab0ed2438d
PimAkker <pimjanvandenakker@gmail.com>
2024-07-09 11:41:30 +0200

    temp

--- blender_python_code/data_gen_utils.py
+++ blender_python_code/data_gen_utils.py
@@ -346,14 +346,9 @@ class blender_object_placement:
         bpy.data.objects[object_name].select_set(True)
         # set object as active
         bpy.context.view_layer.objects.active = bpy.data.objects[object_name]
-        obj = bpy.context.active_object
+        obj = bpy.context.active_object      
 
-
-
-        
-        tic = time.time()
         bpy.ops.object.duplicate_move(OBJECT_OT_duplicate={"linked":False, "mode":'TRANSLATION'}, TRANSFORM_OT_translate={"value":(45.2641, 3.86619, -3.15708), "orient_type":'GLOBAL', "orient_matrix":((1, 0, 0), (0, 1, 0), (0, 0, 1)), "orient_matrix_type":'GLOBAL', "constraint_axis":(False, False, False), "mirror":False, "use_proportional_edit":False, "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "use_proportional_connected":False, "use_proportional_projected":False, "snap":False, "snap_elements":{'FACE_NEAREST'}, "use_snap_project":True, "snap_target":'CLOSEST', "use_snap_self":True, "use_snap_edit":True, "use_snap_nonedit":True, "use_snap_selectable":False, "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "cursor_transform":False, "texture_space":False, "remove_on_cancel":False, "view2d_edge_pan":False, "release_confirm":False, "use_accurate":False, "use_automerge_and_split":False})
-        print(f'------------------{time.time()-tic}------------------')
 
         bpy.ops.object.convert(target='MESH')
         bpy.context.view_layer.objects.active = bpy.data.objects[f'{object_name}.001']
